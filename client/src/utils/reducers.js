beb255b94960d7571898f05a4cb84094a259877b
NavaHub <navaa003@gmail.com>
2023-11-10 18:54:51 -0600

    Implemented actions and reducers codework

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,5 +3,56 @@ import{
     UPDATE_COMMENT,
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY,
+    REMOVE_POST,
+    REMOVE_COMMENT,
     TOGGLE_DONATION
 } from "./actions";
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_POST:
+            return {
+                ...state,
+                post: [...action.post]
+            };
+    
+        case UPDATE_COMMENT:
+            return {
+                ...state,
+                comment: [...action.comment]
+            };
+
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
+            };
+
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            };
+
+        case REMOVE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter((post) => post._id !== action.payload)
+            };
+
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                comment: state.comments.filter((comment) => comment.id !== action.payload)
+            };
+        
+        case TOGGLE_DONATION:
+            return {
+                ...state,
+                donateOpen: !state.donateOpen
+            };
+        
+        default:
+            return state;
+    }
+}
